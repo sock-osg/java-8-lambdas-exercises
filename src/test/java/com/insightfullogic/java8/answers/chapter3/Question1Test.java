bd7e1ee72f28197b1c6174f9480514f628079ebd
Samuel Quintana <sock.sqt@gmail.com>
2020-05-02 11:43:18 -0500

    Pending chapter's 3 questions

--- src/test/java/com/insightfullogic/java8/answers/chapter3/Question1Test.java
+++ src/test/java/com/insightfullogic/java8/answers/chapter3/Question1Test.java
@@ -2,10 +2,11 @@ package com.insightfullogic.java8.answers.chapter3;
 
 import com.insightfullogic.java8.examples.chapter1.Album;
 import com.insightfullogic.java8.examples.chapter1.SampleData;
-import com.insightfullogic.java8.answers.chapter3.Question1;
+import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
 
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static com.insightfullogic.java8.examples.chapter1.SampleData.*;
@@ -16,19 +17,36 @@ public class Question1Test {
 
     @Test
     public void addsEmptyList() {
-        int result = com.insightfullogic.java8.answers.chapter3.Question1.addUp(Stream.empty());
-        assertEquals(0, result);
+        int result = Question1.addUp(Stream.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+        assertEquals(55, result);
+    }
+
+    @Test
+    public void concatenateString() {
+        Stream<String> originalArray = Stream.of("S", "A", "M", "U", "E", "L");
+        String result = originalArray.reduce(StringUtils.EMPTY, (acc, currentChar) -> acc + currentChar);
+        assertEquals("SAMUEL", result);
     }
 
     @Test
     public void addsListWithValues() {
-        int result = com.insightfullogic.java8.answers.chapter3.Question1.addUp(Stream.of(1, 3, -2));
+        int result = Question1.addUp(Stream.of(1, 3, -2));
         assertEquals(2, result);
     }
 
     @Test
     public void extractsNamesAndOriginsOfArtists() {
-        List<String> namesAndOrigins = com.insightfullogic.java8.answers.chapter3.Question1.getNamesAndOrigins(SampleData.getThreeArtists());
+        List<String> namesAndOrigins = Question1.getNamesAndOrigins(SampleData.getThreeArtists());
+        assertEquals(asList("John Coltrane", "US", "John Lennon", "UK", "The Beatles", "UK"), namesAndOrigins);
+    }
+
+    @Test
+    public void extractsNamesAndOriginsOfArtistsMine() {
+        List<String> result = SampleData.getThreeArtists().stream()
+            .flatMap(artist -> Stream.of(artist.getName(), artist.getNationality()))
+            .collect(Collectors.toList());
+
+        List<String> namesAndOrigins = Question1.getNamesAndOrigins(SampleData.getThreeArtists());
         assertEquals(asList("John Coltrane", "US", "John Lennon", "UK", "The Beatles", "UK"), namesAndOrigins);
     }
 
@@ -39,4 +57,12 @@ public class Question1Test {
         assertEquals(asList(sampleShortAlbum, aLoveSupreme), result);
     }
 
+    @Test
+    public void findsShortAlbumsMine() {
+        List<Album> input = asList(manyTrackAlbum, sampleShortAlbum, aLoveSupreme);
+        List<Album> result = input.stream()
+            .filter(album -> album.getTracks().count() <= 3)
+            .collect(Collectors.toList());
+        assertEquals(asList(sampleShortAlbum, aLoveSupreme), result);
+    }
 }
--- src/test/java/com/insightfullogic/java8/answers/chapter3/Question2Test.java
+++ src/test/java/com/insightfullogic/java8/answers/chapter3/Question2Test.java
@@ -1,9 +1,11 @@
 package com.insightfullogic.java8.answers.chapter3;
 
+import com.insightfullogic.java8.examples.chapter1.Artist;
 import com.insightfullogic.java8.examples.chapter1.SampleData;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.List;
 
 import static com.insightfullogic.java8.answers.chapter3.Question2.countBandMembersInternal;
 import static org.junit.Assert.assertEquals;
@@ -15,4 +17,11 @@ public class Question2Test {
         assertEquals(4, countBandMembersInternal(Arrays.asList(SampleData.johnColtrane, SampleData.theBeatles)));
     }
 
+    @Test
+    public void internalIterationMine() {
+        List<Artist> artists = Arrays.asList(SampleData.johnColtrane, SampleData.theBeatles);
+        long result = artists.stream().map(artist -> artist.getMembers().count()).reduce(0L, Long::sum);
+
+        assertEquals(4L, result);
+    }
 }
